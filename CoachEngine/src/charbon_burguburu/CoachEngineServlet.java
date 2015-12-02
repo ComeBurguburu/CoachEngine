dfb1807ece83c6322767b9e6fde0982c8adf7de9
CômeB <come.burguburu@gmail.com>
2015-12-02 08:23:57 +0100

    init

---
+++ CoachEngine/src/charbon_burguburu/CoachEngineServlet.java
@@ -0,0 +1,11 @@
+package charbon_burguburu;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class CoachEngineServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
