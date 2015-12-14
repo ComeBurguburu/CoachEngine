67b8f21c2c9c3ba8957bbb19b8093bcab956861d
CômeB <come.burguburu@gmail.com>
2015-12-14 14:02:56 +0100

    OAuth

---
+++ CoachEngine/war/js/OAuth.js
@@ -0,0 +1,28 @@
+function hash() {
+    var obj = {};
+   $(location).prop("hash").slice(1).split("&").forEach(function (param) {var tab = param.split("="); if (tab.length === 2) {if (obj[tab[0]] === undefined) {obj[tab[0]] = tab[1]; } else {if (typeof obj[tab[0]] !== "object") {obj[tab[0]] = [obj[tab[0]]]; obj[tab[0]].push(tab[1]); } } } });
+   return obj
+}
+var _hash = hash();
+if(_hash.access_token !== undefined){
+	$(".connectGoogle").toggle();
+}
+var access_token = _hash.access_token;
+var url = 'https://www.googleapis.com/plus/v1/people/me?access_token='+access_token;
+if(access_token!==undefined){
+$.ajax({
+  type: 'GET',
+  url: url,
+  success: function(userInfo) {
+    //info about user
+    console.log(userInfo);
+    $(".name").html(userInfo.displayName)
+    $(".pict").prop("src",userInfo.image.url);
+    $(".email").html(userInfo.emails[0].value);
+  },
+  error: function(e) {
+    console.log('error');
+
+  }
+});
+}
---
+++ CoachEngine/war/js/search.js
@@ -0,0 +1,4 @@
+$(".form-control").on("keydown keyup",function(){
+	console.log($(this).val());
+})
+//$(".form-control").val("rr");
