76bc2a874081f217ae3b8a42cd7a11eef6eb9eb8
CômeB <come.burguburu@gmail.com>
2015-12-15 14:58:25 +0100

    remove duplicate flipcounter

--- CoachEngine/war/js/details.js
+++ CoachEngine/war/js/details.js
@@ -5,15 +5,15 @@ $.get("/addtraining",function(response){
 	console.log(json);
 	window.test = json;
 	var index = 0;
+	var block = $("tr:eq(0)").clone();
 	$.each(json.Exercice, function(key, value){
 		setTitle(index, value.Title);
 		setTime(index, value.Date);
 		setDescription(index,value.Description);
-		alert(JSON.stringify(value));
-		//setFlip(index,value.Date);
+		setFlip(index,value.Date);
 		index++;
-		if(index < json.Exercice.length - 1){
-			$("table").append($("tr:eq(0)").clone());
+		if(index < json.Exercice.length){
+			$("table").append(block.clone());
 		}
 	});
 	
@@ -24,7 +24,6 @@ function setTitle(index, value){
 }
 function setTime(index, value){
 	$(".time-exercice").eq(index).text(value);
-	setFlip(index,value);
 }
 function setDescription(index, value){
 	$(".description-exercice").eq(index).text(value);
@@ -32,7 +31,7 @@ function setDescription(index, value){
 function setFlip(index, value){
 	console.assert(value!=undefined);
 	var elem = $(".flip").eq(index);
-	console.log('5.10.2012 '+value);
+	console.log(index+': 5.10.2012 '+value);
 	var date=new Date('5.10.2012 '+value);
   var  time=date.getTime();
 
@@ -42,4 +41,5 @@ function setFlip(index, value){
              	date=new Date(currentTime);
              return date;
          }});
+    console.log(elem);
 }
