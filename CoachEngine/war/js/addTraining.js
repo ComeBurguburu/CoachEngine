9eda3474788e647d0e2d2a00a2326481b9538c7b
HuguesCharbon <charbonhugues@gmail.com>
2015-12-15 11:54:23 +0100

    upadateAddTraining

--- CoachEngine/war/js/addTraining.js
+++ CoachEngine/war/js/addTraining.js
@@ -4,14 +4,17 @@ tab.Exercice=[];
 DateOKLM = 0;
 
 
+
 $("#validate").click(function() {
 	console.log("coucou");
 	tab.Title=$("#inputTitle").val();
 	tab.Description=$("#inputDescription").val();
 	tab.Domain=$("#e1").val();
 	tab.Date = DateOKLM;
+
 	$.post("/addtraining",{training: JSON.stringify(tab)},function(response){alert(response);});
 	console.log("after ajax call")
+
 	
 });
 
@@ -28,6 +31,9 @@ Number.prototype.toHHMMSS = function () {
     return time;
 }
 
+//var tabId=[];
+//var id=0;
+
 $("#buttonAddEx").click(function() {
   
 	
@@ -43,6 +49,8 @@ $("#buttonAddEx").click(function() {
 	DateTmp=parseInt(currentExo.Hours,10)*3600+parseInt(currentExo.Minutes,10)*60+parseInt(currentExo.Seconds,10)
 	DateOKLM=DateOKLM+DateTmp;
 	
+	
+	
 	/*
 	console.log(Exercice[0].Title);
 	console.log(Exercice[0].Description);
@@ -51,17 +59,49 @@ $("#buttonAddEx").click(function() {
 	console.log(Exercice[0].Seconds);
 	
 	*/
+	//listExercices
+	//tabId[id]=currentExo.Title;
 	
-	var Html="<tr><td>1</td><td>"+currentExo.Title+"</td><td class='hidden-xs'><p>"+currentExo.Description+"</p></td><td>"+currentExo.Hours+" Hours "+currentExo.Minutes+" Minutes "+currentExo.Seconds+" Seconds </td><td><button type='submit' class='btn btn-danger btn-sm'><span class='glyphicon glyphicon-remove'></span></button></td></tr><tr>";
+	//console.log(tabId[id]);
+	//console.log("#"+tabId[id]+"ButtonRemove");
+	
+	var Html="<tr id='"+currentExo.Title+"'><td>1</td><td>"+currentExo.Title+"</td><td class='hidden-xs'><p>"+currentExo.Description+"</p></td><td>"+currentExo.Hours+" Hours "+currentExo.Minutes+" Minutes "+currentExo.Seconds+" Seconds </td><td><button id='"+currentExo.Title+"ButtonRemove' type='submit' class='btn btn-danger btn-sm'><span class='glyphicon glyphicon-remove'></span></button></td></tr><tr>";
 	$("#listExercices").append(Html);
+	
+	$("#totalTime").remove();
+	$("#totalTimeValue").append("<div id='totalTime'>"+DateOKLM.toHHMMSS()+"</div>");
 	
 	tab.Exercice.push(currentExo);
 	console.log(tab.Exercice[0]);
 	
+
+	//for(var i in tabId)
+	//{
+	
+	$("#"+currentExo.Title+"ButtonRemove").click(function() {
+		console.log("Button Remove");
+		var grandPere = $(this).parent().parent();
+		var id=grandPere.index();
+		tab.Exercice.splice(id,1);
+		grandPere.remove();
+	//	tab.Exercice.sl(currentExo);
+		//var x=$(this).data(id);
+		
+	});
+
+	
+	
+	//}
 	$("#titleDescription").val("");
 	$("#exerciceDescription").val("");
 	$("#hours").val("");
 	$("#minutes").val("");
 	$("#seconds").val("");
-	
+
 });
+
+$("#refreshAll").click(function() {
+location.reload();
+
+});
+
